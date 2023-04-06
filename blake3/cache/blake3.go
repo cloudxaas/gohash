881cb7111aa971a286ea98ef9e51867c0f3a49a8
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-06 09:50:59 -0700

    Update blake3.go

--- blake3/cache/blake3.go
+++ blake3/cache/blake3.go
@@ -17,6 +17,21 @@ func New(size int) *Cache {
 
 func (c *Cache) Hash(key []byte) []byte {
 	// Check if the key exists in the cache
+	if v, ok := c.cache.Get(key); ok {
+		return v
+	}
+
+	// Compute the Blake3 hash of the key
+  hash := blake3.Sum256(k)
+
+	// Add the hash to the cache
+  c.cache.Set(key, hash[:])
+
+  return hash[:]
+}
+
+func (c *Cache) HashBig(key []byte) []byte {
+	// Check if the key exists in the cache
 	if v, ok := c.cache.GetBig(key); ok {
 		return v
 	}
