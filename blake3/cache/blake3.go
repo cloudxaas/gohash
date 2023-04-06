10002a259af3319e61509f547fba7e76e6969079
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-06 09:49:43 -0700

    Create blake3.go

---
+++ blake3/cache/blake3.go
@@ -0,0 +1,31 @@
+package blake3cache
+
+import (
+	"github.com/VictoriaMetrics/fastcache"
+	"github.com/zeebo/blake3"
+)
+
+type Cache struct {
+	cache *fastcache.Cache
+}
+
+func New(size int) *Cache {
+	return &Cache{
+		cache: fastcache.New(size),
+	}
+}
+
+func (c *Cache) Hash(key []byte) []byte {
+	// Check if the key exists in the cache
+	if v, ok := c.cache.Get(key); ok {
+		return v
+	}
+
+	// Compute the Blake3 hash of the key
+  hash := blake3.Sum256(k)
+
+	// Add the hash to the cache
+  c.cache.Set(key, hash[:])
+
+  return hash[:]
+}
