a1f1d4ba4d5786f1813777836638d723c79e93a2
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-07 12:18:34 -0700

    Update fnv1a64.go

--- fnv1a64/fnv1a64.go
+++ fnv1a64/fnv1a64.go
@@ -1,2 +1,15 @@
 package fnv1a64
 
+const (
+    offset64 = 14695981039346656037
+    prime64  = 1099511628211
+)
+
+func Hash64(data []byte) uint64 {
+    var h uint64 = offset64
+    for _, b := range data {
+        h ^= uint64(b)
+        h *= prime64
+    }
+    return h
+}
